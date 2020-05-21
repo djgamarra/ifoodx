b0eddb28034f913fa4f7788a9f8a13749e24f8aa
Jose David Padilla Gamarra <djgamarra@uninorte.edu.co>
2020-05-21 07:39:21 -0500

    ok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,8 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-
+  config.action_cable.url = [/ws:\/\/*/, /wss:\/\/*/]
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
