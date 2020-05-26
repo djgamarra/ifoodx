5c8c8d9f09b86183ed6329c63d4f7e4e723936ab
Jose David Padilla Gamarra <djgamarra@uninorte.edu.co>
2020-05-26 14:25:43 -0500

    Product finished

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,62 +2,62 @@ class OrdersController < ApplicationController
   before_action :require_logged_in
 
   def index
-    @orders      = if @current_user.is_admin
-                     Order.preload(:ingredients).all
-                   else
-                     @current_user.orders.preload(:ingredients)
-                   end
+    @orders = if @current_user.is_admin
+                Order.preload(:ingredients).all
+              else
+                @current_user.orders.preload(:ingredients)
+              end
     @ingredients = Ingredient.all
-    ing          = {}
+    ing = {}
     @ingredients.each { |i| ing[i.id] = i }
     gon.push ingredients: ing
   end
 
   def show
-    @order = Order.preload(:ingredients, messages: :user).find_by(id: params[:id])
+    @order = Order.includes(:ingredients, messages: :user).find_by(id: params[:id])
   end
 
   def new
-    @order       = Order.new
+    @order = Order.new
     @ingredients = Ingredient.all
-    ing          = {}
+    ing = {}
     @ingredients.each { |i| ing[i.id] = i }
     gon.push ingredients: ing
   end
 
   def create
-    p                 = order_params
-    p[:user]          = @current_user
+    p = order_params
+    p[:user] = @current_user
     p[:minimum_price] = p[:price]
-    @order            = Order.create p
+    @order = Order.create p
     (params.require(:order).permit(ingredients: {})[:ingredients] || {}).each do |k, v|
       @order.order_ingredients.create ingredient_id: k.to_i, amount: v.to_i
     end
     if @current_user.is_admin
       @orders = Order.preload(:ingredients).all
       ActionCable.server.broadcast "user", body: {
-          general:  render_to_string(partial: 'layouts/p1', locals: { admin: true }),
-          specific: render_to_string(partial: 'layouts/p2'),
-          id:       params[:id],
+        general: render_to_string(partial: 'layouts/p1', locals: { admin: true }),
+        specific: render_to_string(partial: 'layouts/p2'),
+        id: params[:id],
       }
       @orders = Order.where(user_id: @order.user_id).preload(:ingredients).all
       ActionCable.server.broadcast "user_#{@order.user_id}", body: {
-          general:  render_to_string(partial: 'layouts/p1', locals: { admin: false }),
-          specific: render_to_string(partial: 'layouts/p2'),
-          id:       params[:id],
+        general: render_to_string(partial: 'layouts/p1', locals: { admin: false }),
+        specific: render_to_string(partial: 'layouts/p2'),
+        id: params[:id],
       }
     else
       @orders = Order.preload(:ingredients).all
       ActionCable.server.broadcast "user", body: {
-          general:  render_to_string(partial: 'layouts/p1', locals: { admin: true }),
-          specific: render_to_string(partial: 'layouts/p2'),
-          id:       params[:id],
+        general: render_to_string(partial: 'layouts/p1', locals: { admin: true }),
+        specific: render_to_string(partial: 'layouts/p2'),
+        id: params[:id],
       }
       @current_user.orders.preload(:ingredients)
       ActionCable.server.broadcast "user_#{@current_user.id}", body: {
-          general:  render_to_string(partial: 'layouts/p1', locals: { admin: false }),
-          specific: render_to_string(partial: 'layouts/p2'),
-          id:       params[:id],
+        general: render_to_string(partial: 'layouts/p1', locals: { admin: false }),
+        specific: render_to_string(partial: 'layouts/p2'),
+        id: params[:id],
       }
     end
     redirect_to order_path @order
@@ -68,35 +68,35 @@ class OrdersController < ApplicationController
   end
 
   def update
-    p      = params.require(:order).permit(:state, :price)
+    p = params.require(:order).permit(:state, :price)
     @order = Order.preload(:ingredients).find_by(id: params[:id])
     @order.update p
     @order = Order.preload(:ingredients).find_by(id: params[:id])
     if @current_user.is_admin
       @orders = Order.preload(:ingredients).all
       ActionCable.server.broadcast "user", body: {
-          general:  render_to_string(partial: 'layouts/p1', locals: { admin: true }),
-          specific: render_to_string(partial: 'layouts/p2'),
-          id:       params[:id],
+        general: render_to_string(partial: 'layouts/p1', locals: { admin: true }),
+        specific: render_to_string(partial: 'layouts/p2'),
+        id: params[:id],
       }
       @orders = Order.where(user_id: @order.user_id).preload(:ingredients).all
       ActionCable.server.broadcast "user_#{@order.user_id}", body: {
-          general:  render_to_string(partial: 'layouts/p1', locals: { admin: false }),
-          specific: render_to_string(partial: 'layouts/p2'),
-          id:       params[:id],
+        general: render_to_string(partial: 'layouts/p1', locals: { admin: false }),
+        specific: render_to_string(partial: 'layouts/p2'),
+        id: params[:id],
       }
     else
       @orders = Order.preload(:ingredients).all
       ActionCable.server.broadcast "user", body: {
-          general:  render_to_string(partial: 'layouts/p1', locals: { admin: true }),
-          specific: render_to_string(partial: 'layouts/p2'),
-          id:       params[:id],
+        general: render_to_string(partial: 'layouts/p1', locals: { admin: true }),
+        specific: render_to_string(partial: 'layouts/p2'),
+        id: params[:id],
       }
       @current_user.orders.preload(:ingredients)
       ActionCable.server.broadcast "user_#{@current_user.id}", body: {
-          general:  render_to_string(partial: 'layouts/p1', locals: { admin: false }),
-          specific: render_to_string(partial: 'layouts/p2'),
-          id:       params[:id],
+        general: render_to_string(partial: 'layouts/p1', locals: { admin: false }),
+        specific: render_to_string(partial: 'layouts/p2'),
+        id: params[:id],
       }
     end
     redirect_to edit_order_path @order
@@ -106,10 +106,14 @@ class OrdersController < ApplicationController
     @order = Order.preload(:ingredients, messages: :user).find_by(id: message_params[:order_id])
     @order.messages.create message_params
     ActionCable.server.broadcast "user_#{@order.user_id}", body: {
-        chat: render_to_string(partial: 'layouts/chat')
+      chat: render_to_string(partial: 'layouts/chat'),
+      notice: @current_user.is_admin,
+      order_id: @order.id
     }
     ActionCable.server.broadcast "user", body: {
-        chat: render_to_string(partial: 'layouts/chat')
+      chat: render_to_string(partial: 'layouts/chat'),
+      notice: !@current_user.is_admin,
+      order_id: @order.id
     }
   end
 
@@ -120,6 +124,8 @@ class OrdersController < ApplicationController
   end
 
   def message_params
-    params.require(:message).permit :text, :user_id, :order_id
+    p = params.require(:message).permit :text, :order_id
+    p[:user_id] = @current_user.id
+    p
   end
 end
--- app/javascript/channels/order_channel.js
+++ app/javascript/channels/order_channel.js
@@ -12,19 +12,22 @@ consumer.subscriptions.create({channel: "OrderChannel", user_id: gon.user_id}, {
         const paths = window.location.href.split("/").reverse()
         const notice = document.getElementById('notice').MDCSnackbar;
         notice.labelText = "Ha recibido una actualización en un pedido";
-        if (paths[0] === "orders") {
-            document.getElementById("orders").outerHTML = body["general"]
-            notice.open()
-        } else if (paths[0] === body["id"]) {
-            document.getElementById("orders").outerHTML = body["specific"]
-            notice.open()
+        if (body["chat"]) {
+            if (window.location.href.includes(`/orders/${body["order_id"]}`)) {
+                if (document.getElementById("chat").className.includes("opened")) body["chat"] = body["chat"].replace(`class="closed shaped"`, `class="opened shaped"`)
+                document.getElementById("chat").outerHTML = body["chat"]
+            }
+            notice.labelText = "Ha recibido un mensaje";
+            if (body["notice"]) notice.open()
+        } else {
+            if (paths[0] === "orders") {
+                document.getElementById("orders").outerHTML = body["general"]
+                notice.open()
+            } else if (paths[0] === body["id"]) {
+                document.getElementById("orders").outerHTML = body["specific"]
+                notice.open()
+            }
         }
         console.log(body)
-        if (body["chat"]) {
-            if (document.getElementById("chat").className.includes("opened")) body["chat"] = body["chat"].replace(`class="closed shaped"`, `class="opened shaped"`)
-            document.getElementById("chat").outerHTML = body["chat"]
-            notice.labelText = "Ha recibido un mensaje";
-            notice.open()
-        }
     }
 });
